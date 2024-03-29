c40c53f31030303d4681b885beadd84653ed2c34
Swati Badola <102805192+swatibadola@users.noreply.github.com>
2024-03-29 12:38:57 +0530

    app

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,9 +2,16 @@
 import Demo from "./demo"
 
 function App() {
+  const username = "Swati aur Code"
 
   return (
+    <>
     <Demo/>
+    <h1>Welcome to my platform username</h1>
+    <h1>Welcome to my platform {username}</h1> 
+    {/* {username} is called an evaluated expression */}
+    </>
+
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,48 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+function MyApp(){
+  return(
+    <div> 
+      <h1>Welcome to Sapno ka Mela</h1>
+    </div>
+
+  )
+}
+
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit GoogleBaba'
+// }
+
+
+const AnotherElement = (
+  <a href="https://google.com" target='_blank'>You are visiting GoogleBaba</a>
+)
+
+const anotherUser = "Web Developer" 
+
+
+const ReactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Press me to redirect to GoogleBaba',
+  anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    // <MyApp/> //Here MyApp is a function.
+    // MyApp()
+
+
+    
+    ReactElement
+    // AnotherElement
+    // ReactElement //As ReactElement is an object therefore, it is accessed directly.
+    // <ReactElement/>
 )
