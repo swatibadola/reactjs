a2435a341af496e75dd8da048ef54b0b8917557a
Swati Badola <swatibadola39@gmail.com>
2024-05-19 12:12:49 +0530

    themeswitcher

--- 11themeswitch/tailwind.config.js
+++ 11themeswitch/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js, jsx}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
+
 
