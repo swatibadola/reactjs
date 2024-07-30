9a3d0d1427c95ef7f64c9209215c7eb16159211e
Swati Badola <swatibadola39@gmail.com>
2024-07-30 22:56:23 +0530

    Changes done

--- 12todolist/src/App.js
+++ 12todolist/src/App.js
@@ -43,7 +43,7 @@ function App() {
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, ToggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Conquer Your Todos : Because Life Needs a Checklist</h1>
           <div className="mb-4">
             {/* Todo form goes here */}
             <TodoForm/>
